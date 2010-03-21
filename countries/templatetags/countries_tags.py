f60988ecdc4991bae395d354147c04b07a7fd3e4
Rob Hudson <rob@cogit8.org>
2010-03-21 13:52:15 -0700

    Updated to use decorator syntax for `stringfilter`.

--- countries/templatetags/countries_tags.py
+++ countries/templatetags/countries_tags.py
@@ -3,6 +3,7 @@ from django.template.defaultfilters import stringfilter
 
 register = template.Library()
 
+@stringfilter
 def iso_flag(iso, flag_path=u''):
     """
     Returns a full path to the ISO 3166-1 alpha-2 country code flag image.
@@ -16,7 +17,5 @@ def iso_flag(iso, flag_path=u''):
     """
     from countries.utils.isoflag import iso_flag
     return iso_flag(iso, flag_path)
-iso_flag = stringfilter(iso_flag)
 
-# Syntax: register.filter(name of filter, callback)
 register.filter('iso_flag', iso_flag)
